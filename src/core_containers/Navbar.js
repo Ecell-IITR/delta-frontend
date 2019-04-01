709960317a454fda8d0e0fb56ebd3eed13363fb2
srivastava9 <krishu4626@gmail.com>
2019-04-01 19:48:43 +0530

    made navbar file for navbar

---
+++ src/core_containers/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Searchbar from "./searchbar";
+import ImageIndex from "./image";
+import "./navbar.css";
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="navbar">
+        <div className="title">DELTA</div>
+        <Searchbar />
+        <div className="subnavbar">
+          <ul>
+            <li>
+              <a href="" className="profile">
+                Profile
+              </a>
+            </li>
+            <li>
+              <a href="" className="opportunities">
+                Opportunities
+              </a>
+            </li>{" "}
+            <li>
+              <a href="" className="createpost">
+                Create Post
+              </a>
+            </li>{" "}
+            <li>
+              <a href="" className="notification">
+                Notification
+              </a>
+            </li>{" "}
+            <li>
+              <a href="" className="more">
+                More
+              </a>
+            </li>
+          </ul>
+          <ImageIndex image="" size="" shape="circular" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
