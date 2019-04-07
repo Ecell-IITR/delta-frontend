9cb032531ec407af480e0661cb1cec3c67006ca4
prakhar2109 <prakharagarwal21091998@gmail.com>
2019-04-07 07:00:12 +0530

    merge conflict resolve

--- src/student/components/index.jsx
+++ src/student/components/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Login from "./login/index";
-<<<<<<< HEAD
+import Navbar from '../../core_containers/Navbar/Navbar.js'
 import { Provider } from 'react-redux';
 import { createStore} from 'redux';
 import reducer from '../reducers/index'
@@ -13,27 +13,11 @@ class StudentIndex extends Component {
   render() {
     return (
       <div>
-        <Provider store={store}><Login /></Provider>
+        <Provider store={store}><Navbar/><Login /></Provider>
+        
       </div>
     );
   }
-=======
-import Navbar from '../../core_containers/Navbar/Navbar.js'
-
-class StudentIndex extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <Login />
-            </div>
-        );
-    }
->>>>>>> e11562acee4553631b18e437d1dd1d6fbcbc97dd
 }
 
 export default StudentIndex;
