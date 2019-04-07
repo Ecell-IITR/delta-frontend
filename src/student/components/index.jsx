a8fb9984c245f397ed0146b0dcd29cfea63a6ddb
prakhar2109 <prakharagarwal21091998@gmail.com>
2019-04-07 06:47:21 +0530

    setting redux in student and company app

--- src/student/components/index.jsx
+++ src/student/components/index.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import Login from "./login/index";
+import { Provider } from 'react-redux';
+import { createStore} from 'redux';
+import reducer from '../reducers/index'
+const store = createStore(reducer);
 class StudentIndex extends Component {
   constructor(props) {
     super(props);
@@ -8,7 +12,7 @@ class StudentIndex extends Component {
   render() {
     return (
       <div>
-        <Login />
+        <Provider store={store}><Login /></Provider>
       </div>
     );
   }
