b93a44cd000fa09beb55cb04b030e31d02f7d02e
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-05-14 15:36:55 +0530

    feat:custom font added

---
+++ src/core_containers/label/label.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Icon, Label } from 'semantic-ui-react'
+export default class label extends Component {
+
+  render() {
+    return (
+      <div>
+        <Label image>
+          {this.props.labelName}
+          <Icon name='delete' />
+        </Label>
+      </div>
+    )
+  }
+}
