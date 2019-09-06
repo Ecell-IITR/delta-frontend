ea18f6f29415c0561ed7c22c2598a86aed58d02b
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-09-07 03:01:22 +0530

    fix: routing bug fixed

--- src/student/components/app.js
+++ src/student/components/app.js
@@ -1,39 +1,33 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import Register from './register/index'
-import Dashboard from './dashboard/index'
 import Logout from './logout/index'
 import Login from './login/index'
+import AuthenticativeRoutes from './authRoutes'
 import PrivateRoute from './pR'
-import { Navbar } from '../../core_containers'
-import Profile from './profile/index'
-import Opportunities from './opportunities/index'
 
 class StudentIndex extends Component {
   render() {
     const { match } = this.props
     return (
       <React.Fragment>
-        <PrivateRoute path={`${match.path}/`} component={Navbar} />
         <Switch>
           <Route exact path={`${match.path}/login`} component={Login} />
           <Route exact path={`${match.path}/logout`} component={Logout} />
           <Route exact path={`${match.path}/register`} component={Register} />
-          <PrivateRoute exact path={`${match.path}/`} component={Dashboard} />
           <PrivateRoute
-            exact
-            path={`${match.path}/profile`}
-            component={Profile}
-          />
-          <PrivateRoute
-            exact
-            path={`${match.path}/opportunities`}
-            component={Opportunities}
+            path={`${match.path}/`}
+            component={AuthenticativeRoutes}
           />
         </Switch>
       </React.Fragment>
     )
   }
+}
+
+StudentIndex.propTypes = {
+  match: PropTypes.object
 }
 
 export default StudentIndex
---
+++ src/student/components/authRoutes.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import Dashboard from './dashboard/index'
+import { Navbar } from '../../core_containers'
+import Profile from './profile/index'
+import Opportunities from './opportunities/index'
+
+class AuthenticativeRoutes extends Component {
+  render() {
+    const { match } = this.props
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Route exact path={`${match.path}/`} component={Dashboard} />
+        <Route exact path={`${match.path}profile`} component={Profile} />
+        <Route
+          exact
+          path={`${match.path}opportunities`}
+          component={Opportunities}
+        />
+      </React.Fragment>
+    )
+  }
+}
+
+AuthenticativeRoutes.propTypes = {
+  match: PropTypes.object
+}
+
+export default AuthenticativeRoutes
