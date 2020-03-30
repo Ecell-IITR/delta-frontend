6f8832ae431f88086450db75cd3a40ef47037bc4
Yash Srivastava <52625656+darkshredder@users.noreply.github.com>
2020-03-30 19:52:05 +0530

    fixing console warnings

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { Provider } from "react-redux"
-import { store } from "./utils/helpers/configStore"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './utils/helpers/configStore'
 import { ToastProvider } from 'react-toast-notifications'
-import "./index.css"
-import "semantic-ui-css/semantic.min.css"
-import App from "./App"
-import * as serviceWorker from "./serviceWorker"
+import './index.css'
+import 'semantic-ui-css/semantic.min.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 ReactDOM.render(
-	<Provider store={store}>
-		<ToastProvider>
-			<App />
-		</ToastProvider>
-	</Provider>,
-	document.getElementById("root")
+  <Provider store={store}>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
+  </Provider>,
+  document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change
