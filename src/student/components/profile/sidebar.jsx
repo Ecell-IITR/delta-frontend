2fb34db0bc452cfb110fc3678e94d3476129697b
Darkshredder <darkshredder@Darkshredders-MacBook-Air.local>
2020-04-12 06:06:08 +0530

    styling of profile lower half page

--- src/student/components/profile/sidebar.jsx
+++ src/student/components/profile/sidebar.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import SideBarRow from '../../../coreContainers/sideBarRow'
-import "../css/profileSidebar.css"
+import styles_2 from "../css/profileSidebar.module.css"
 class ProfileSidebar extends Component {
   state = {}
   render() {
     return (
-      <div className="profile-sidebar">
+      <div className={styles_2.profilesidebar}>
         <SideBarRow Icon="th list" Title="Posts" />
-        <SideBarRow Icon="id card outline" Title="Posts" />
+        <Link to="/profile/skills">
+        <SideBarRow Icon="id card outline" Title="Skills" />
+        </Link>
         <SideBarRow Icon="trophy" Title="Achievements" />
+        <Link to="/profile/resume">
         <SideBarRow Icon="file alternate" Title="Resume" />
+        </Link>
       </div>
     )
   }
