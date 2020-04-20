8b5b52f8662050a9b5ea0064496686c3463effc5
RupinSamria <rupin.samria@gmail.com>
2020-04-21 04:57:48 +0530

    routeCreatePost

--- src/student/components/createpost/index.js
+++ src/student/components/createpost/index.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import CreatePostSidebar from './sidebar'
+import { Switch, Route } from 'react-router-dom'
 import Internship from './internship'
 
 export class CreatePost extends Component {
     render() {
         return (
             <>
+                <h1>CreatePOst</h1>
                 <div>
                 <CreatePostSidebar />
                 </div>
+                <Switch>
+                  <Route
+                    exact
+                    path={`${match.path}/internship`}
+                    component={React.lazy(() => import('./internship'))}
+                  />
+                  <Route
+                    exact
+                    path={`${match.path}/project`}
+                    component={React.lazy(() => import('./project'))}
+                  />
+                  <Route
+                    exact
+                    path={`${match.path}/competition`}
+                    component={React.lazy(() => import('./competition'))}
+                  />
+                </Switch>
                 <div>
                     <Internship />
                 </div>
