a7a10a9a1fbfba724651e9413487073730c5b989
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-07-14 13:05:49 +0530

    side navbar added

--- src/coreContainers/navbar/index.js
+++ src/coreContainers/navbar/index.js
@@ -7,7 +7,15 @@ import { TOKEN_TYPE } from 'globalConstants'
 // import logo from '../../images/logo.svg'
 // import Searchbar from '../searchbar/index'
 import { getImageURL } from 'utils/getImageURL'
-
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
 import styles from './navbar.css'
 // import PropTypes from 'prop-types'
 class Navbar extends React.PureComponent {
@@ -18,6 +26,44 @@ class Navbar extends React.PureComponent {
     return isLoggedIn ? (
       <div className={styles.navbar}>
         <div className={styles.startnav}>
+        <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
+      <CDBSidebar textColor="#fff" backgroundColor="#2964ee">
+        <CDBSidebarHeader prefix={<div className={styles.title} style={{paddingTop:"1rem"}}>&#9776; </div>}>
+        <div style={{paddingTop:"1rem"}} className={styles.title}>Delta</div>
+        </CDBSidebarHeader>
+
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+            <NavLink exact to="/" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/tables" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/profile" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/analytics" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
+            </NavLink>
+
+            <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="exclamation-circle">404 page</CDBSidebarMenuItem>
+            </NavLink>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+
+        <CDBSidebarFooter style={{ textAlign: 'center' }}>
+          <div
+            style={{
+              padding: '20px 5px',
+            }}
+          >
+            Sidebar Footer
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+    </div>
           <div className={styles.title}>Delta</div>
           {/* <div>
             <img src={logo} className={styles.logo} alt="logo" />
